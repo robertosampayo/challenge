77a5e40775365c6ef1263a916e7da4778451d155
robertosampayo <jrobertosampayo@gmail.com>
2020-02-06 19:49:02 -0300

    Updates

---
+++ precache-manifest.20ea2bf1b4123cac9f986013515fbe78.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b34ed6eb65322adc8632ad34665498a",
+    "url": "/challenge/index.html"
+  },
+  {
+    "revision": "11388657cee50a5ec283",
+    "url": "/challenge/static/css/main.f53b6ae4.chunk.css"
+  },
+  {
+    "revision": "012c9c05ee945e6f15b3",
+    "url": "/challenge/static/js/2.37e8ccd6.chunk.js"
+  },
+  {
+    "revision": "0ccca33dfb21399889824ced4d9b459c",
+    "url": "/challenge/static/js/2.37e8ccd6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "11388657cee50a5ec283",
+    "url": "/challenge/static/js/main.1aba9a18.chunk.js"
+  },
+  {
+    "revision": "267858aa5798fe4c1439",
+    "url": "/challenge/static/js/runtime-main.e4ad4cda.js"
+  },
+  {
+    "revision": "7cbdea2a0a6a6d32fa0646cf0f90229b",
+    "url": "/challenge/static/media/banner.7cbdea2a.png"
+  },
+  {
+    "revision": "ed492c24656860077872cd025dd377f4",
+    "url": "/challenge/static/media/default.ed492c24.png"
+  },
+  {
+    "revision": "77a82d5d19e6ce25398b4a5340f2b535",
+    "url": "/challenge/static/media/logo.77a82d5d.svg"
+  },
+  {
+    "revision": "dec62e66cc571f30d1ecf77f0073e5f5",
+    "url": "/challenge/static/media/money.dec62e66.svg"
+  }
+]);
